4c525829c1862ab046e533042fb1c5bbcbed6fcb
Dana Czinsky <DanaCzinsky@gmail.com>
2016-02-13 20:04:34 -0500

    adds timer

--- public/main.js
+++ public/main.js
@@ -2,9 +2,10 @@ console.log('It does not do to dwell on dreams and forget to live');
 
 $(document).ready(function(){
 
-  $('.special.cards .image').dimmer({
-    on: 'hover'
-  });
+  window.setTimeout(function(){
+    $('#scroll').removeClass('hidden');
+    $('#scroll').addClass('animated fadeInDown');
+  }, 1000);
 
   $(window).scroll(function(){
     // console.log($(window).scrollTop())
@@ -20,6 +21,10 @@ $(document).ready(function(){
     if ($(window).scrollTop() < 645) {
       $('#nav-bar').removeClass('navbar-fixed');
     }
+  });
+
+  $('.special.cards .image').dimmer({
+    on: 'hover'
   });
 
 })
