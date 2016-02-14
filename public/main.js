b1042c8722972d2fafbc5287a7477f0f7899bec3
Dana Czinsky <DanaCzinsky@gmail.com>
2016-02-13 19:56:40 -0500

    adds styling, scroll down, animate.css

--- public/main.js
+++ public/main.js
@@ -8,10 +8,16 @@ $(document).ready(function(){
 
   $(window).scroll(function(){
     // console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 375) {
+    if ($(window).scrollTop() > 20) {
+      $('#scroll').addClass('hidden');
+    }
+    if ($(window).scrollTop() < 20) {
+      $('#scroll').removeClass('hidden');
+    }
+    if ($(window).scrollTop() > 645) {
       $('#nav-bar').addClass('navbar-fixed');
     }
-    if ($(window).scrollTop() < 375) {
+    if ($(window).scrollTop() < 645) {
       $('#nav-bar').removeClass('navbar-fixed');
     }
   });
